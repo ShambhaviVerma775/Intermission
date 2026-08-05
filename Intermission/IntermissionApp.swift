accdf077aadefbfe5fd9b1769d177f944f69eaae
ShambhaviVerma775 <shambhaviverma775@gmail.com>
2026-08-06 01:13:20 +0530

    Initial Commit

---
+++ Intermission/IntermissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermissionApp.swift
+//  Intermission
+//
+//  Created by Shambhavi Verma on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct IntermissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IntermissionTests/IntermissionTests.swift
@@ -0,0 +1,17 @@
+//
+//  IntermissionTests.swift
+//  IntermissionTests
+//
+//  Created by Shambhavi Verma on 06/08/26.
+//
+
+import Testing
+@testable import Intermission
+
+struct IntermissionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
